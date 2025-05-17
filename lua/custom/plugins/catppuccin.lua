39674271c9d79340cc9603e41f8bb7d78750fff0
Nicolas-Saavedra <personal@nicolassaavedra.com>
2025-05-16 20:29:51 -0500

    feat: initial customization of kickstart

---
+++ lua/custom/plugins/catppuccin.lua
@@ -0,0 +1,14 @@
+--  Setup for my favorite colorscheme right now; Catppuccin
+return {
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  priority = 1000,
+  config = function()
+    ---@diagnostic disable-next-line: missing-fields
+    require('catppuccin').setup {
+      transparent_background = true,
+    }
+
+    vim.cmd.colorscheme 'catppuccin-mocha'
+  end,
+}
---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,12 @@
+vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  -- Optional dependencies
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
+}
