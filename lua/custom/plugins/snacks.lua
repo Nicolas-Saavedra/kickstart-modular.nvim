c473907f8561fc4993c103493427e1f42194ca62
Nicolas-Saavedra <personal@nicolassaavedra.com>
2025-05-16 21:02:34 -0500

    feat: added snacks nvim to configuration

---
+++ lua/custom/plugins/snacks.lua
@@ -0,0 +1,33 @@
+vim.keymap.set('n', '<Space>lg', '<CMD>lua Snacks.lazygit()<CR>', { desc = 'Open [L]azy[G]it' })
+vim.keymap.set('n', '<Space>gb', '<CMD>lua Snacks.git.blame_line()<CR>', { desc = '[G]it [B]lame the current line' })
+
+--  Communicates Renames to LSP when using Oil.nvim
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'OilActionsPost',
+  callback = function(event)
+    if event.data.actions.type == 'move' then
+      Snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
+    end
+  end,
+})
+
+return {
+  'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    bigfile = { enabled = true },
+    indent = { enabled = true },
+    picker = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    scope = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+    git = { enabled = true },
+    lazygit = { enabled = true },
+    gitbrowse = { enabled = true },
+  },
+}
