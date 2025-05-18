ff966809d2122be327a88a60e34dd8270272f224
Nicolas-Saavedra <personal@nicolassaavedra.com>
2025-05-18 12:01:51 -0500

    chore: adding more formatters and tools

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -239,6 +239,7 @@ return {
             },
           },
         },
+        nil_ls = {},
         jsonls = {},
         sqlls = {},
         terraformls = {},
@@ -272,9 +273,10 @@ return {
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
         'shfmt',
-        'prettier',
+        'prettierd',
         'eslint_d',
         'ruff',
+        'alejandra',
         'checkmake',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
